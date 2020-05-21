423b4471793f52950d4f2757ab9f3d7a663f8d59
marco <marco.daddona@t-online.de>
2020-05-21 08:25:57 +0200

    IndexAction in UebersichtAction ausgeleigert (Frontend Reise) und css in css-datei ausgelagert

--- src/Controllers/Frontend/ReiseController.php
+++ src/Controllers/Frontend/ReiseController.php
@@ -7,12 +7,17 @@ use Entities\Reise;
 class ReiseController extends AbstractBase {
 
 	public function indexAction() {
+
+		//Auf die Ubersicht Seite weiterleiten
+		$this->redirect('uebersicht', 'reise');
+	}
+
+	public function uebersichtAction() {
 		$em = $this->getEntityManager();
 
 		$reisen = $em->getRepository('\Entities\Reise')->findAll();
 
 		$this->addContext('reisen', $reisen);
 	}
-
 
 }
